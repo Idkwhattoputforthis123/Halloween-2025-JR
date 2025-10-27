bef5126e3b1b9076a99294ce3feaf82989802e22
Idkwhattoputforthis123 <cte-andrew.conte@sllboces.org>
2025-10-27 17:52:07 +0000

    day two change

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+// wait for document to be fully loaded
+$(document).ready(function() {
+
+
+
+    function animateFloatingIcons() {
+        //animate the bat
+        $('.bat').animate({
+            top: '+=50px',
+            left: '+=30px'
+        }, 3000, function() {
+            $(this).animate({
+                top: '50px',
+                left: '+=30px'
+        }   , 3000, animateFloatingIcons);
+        });
+
+        // Animate the stars with rotation effect
+        $('.stars').animate({
+            top: '+=40px',
+            right: '+=40px',
+            opacity: 0.8
+        }, 2500, function() {
+            $(this).animate({
+                top: '50px',
+                right: '+=30px',
+                opacity: 0.6
+        }   , 2500,);
+        });
+    }
+    }
+
+};
